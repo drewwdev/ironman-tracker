b9ac889854e59af496d2af28ce71c76dd2a83057
Drew Wilson <jankyy93@gmail.com>
2023-05-17 21:36:50 -0500

    data for range and mage

---
+++ src/data/mageEquipment.ts
@@ -0,0 +1,231 @@
+const mageEquipment = [
+  {
+    id: 1,
+    image: "https://oldschool.runescape.wiki/images/Seers_ring.png?c85b3",
+    tier: "C",
+    name: "Seers Ring",
+    achieved: false,
+  },
+  {
+    id: 2,
+    image: "https://oldschool.runescape.wiki/images/Tome_of_fire.png?c85b3",
+    tier: "C",
+    name: "Tome of Fire",
+    achieved: false,
+  },
+  {
+    id: 3,
+    image: "https://oldschool.runescape.wiki/images/Tome_of_water.png?c85b3",
+    tier: "C",
+    name: "Tome of Water",
+    achieved: false,
+  },
+  {
+    id: 4,
+    image: "https://oldschool.runescape.wiki/images/Mud_battlestaff.png?c85b3",
+    tier: "C",
+    name: "Mud Battlestaff",
+    achieved: false,
+  },
+  {
+    id: 5,
+    image:
+      "https://oldschool.runescape.wiki/images/Smoke_battlestaff.png?c85b3",
+    tier: "C",
+    name: "Smoke Battlestaff",
+    achieved: false,
+  },
+  {
+    id: 6,
+    image:
+      "https://oldschool.runescape.wiki/images/Steam_battlestaff.png?c85b3",
+    tier: "C",
+    name: "Steam Battlestaff",
+    achieved: false,
+  },
+  {
+    id: 7,
+    image: "https://oldschool.runescape.wiki/images/Mist_battlestaff.png?c85b3",
+    tier: "C",
+    name: "Mist Battlestaff",
+    achieved: false,
+  },
+  {
+    id: 8,
+    image: "https://oldschool.runescape.wiki/images/Dust_battlestaff.png?c85b3",
+    tier: "C",
+    name: "Dust Battlestaff",
+    achieved: false,
+  },
+  {
+    id: 9,
+    image: "https://oldschool.runescape.wiki/images/Malediction_ward.png?c85b3",
+    tier: "C",
+    name: "Malediction Ward",
+    achieved: false,
+  },
+  {
+    id: 10,
+    image: "https://oldschool.runescape.wiki/images/Master_wand.png?c85b3",
+    tier: "B",
+    name: "Master Wand",
+    achieved: false,
+  },
+  {
+    id: 11,
+    image: "https://oldschool.runescape.wiki/images/Ahrim%27s_hood.png?c85b3",
+    tier: "B",
+    name: "Ahrim's Hood",
+    achieved: false,
+  },
+  {
+    id: 12,
+    image:
+      "https://oldschool.runescape.wiki/images/Ahrim%27s_robe_top.png?c85b3",
+    tier: "B",
+    name: "Ahrim's Robetop",
+    achieved: false,
+  },
+  {
+    id: 13,
+    image:
+      "https://oldschool.runescape.wiki/images/Ahrim%27s_robe_skirt.png?c85b3",
+    tier: "B",
+    name: "Ahrim's Robeskirt",
+
+    achieved: false,
+  },
+  {
+    id: 14,
+    image: "https://oldschool.runescape.wiki/images/Infinity_boots.png?c85b3",
+    tier: "B",
+    name: "Infinity Boots",
+    achieved: false,
+  },
+  {
+    id: 15,
+    image: "https://oldschool.runescape.wiki/images/Mages%27_book.png?c85b3",
+    tier: "B",
+    name: "Mages' Book",
+    achieved: false,
+  },
+  {
+    id: 16,
+    image: "https://oldschool.runescape.wiki/images/Occult_necklace.png?c85b3",
+    tier: "B",
+    name: "Occult Necklace",
+    achieved: false,
+  },
+
+  {
+    id: 17,
+    image:
+      "https://oldschool.runescape.wiki/images/Staff_of_the_dead.png?c85b3",
+    tier: "B",
+    name: "Staff of the Dead",
+    achieved: false,
+  },
+
+  {
+    id: 18,
+    image:
+      "https://oldschool.runescape.wiki/images/Trident_of_the_seas.png?c85b3",
+    tier: "B",
+    name: "Trident of the Seas",
+    achieved: false,
+  },
+  {
+    id: 19,
+    image: "https://oldschool.runescape.wiki/images/Kodai_wand.png?c85b3",
+    tier: "A",
+    name: "Kodai Wand",
+    achieved: false,
+  },
+  {
+    id: 20,
+    image: "https://oldschool.runescape.wiki/images/Ancestral_hat.png?c85b3",
+    tier: "A",
+    name: "Ancestral Hat",
+    achieved: false,
+  },
+  {
+    id: 21,
+    image:
+      "https://oldschool.runescape.wiki/images/Ancestral_robe_top.png?c85b3",
+    tier: "A",
+    name: "Ancestral Robetop",
+    achieved: false,
+  },
+  {
+    id: 22,
+    image:
+      "https://oldschool.runescape.wiki/images/Ancestral_robe_bottom.png?c85b3",
+    tier: "A",
+    name: "Ancestral Robeskirt",
+    achieved: false,
+  },
+  {
+    id: 23,
+    image:
+      "https://oldschool.runescape.wiki/images/Tormented_bracelet.png?c85b3",
+    tier: "A",
+    name: "Tormented Bracelet",
+    achieved: false,
+  },
+  {
+    id: 24,
+    image:
+      "https://oldschool.runescape.wiki/images/Ancient_wyvern_shield.png?c85b3",
+    tier: "S",
+    name: "Ancient Wyvern Shield",
+    achieved: false,
+  },
+  {
+    id: 25,
+    image:
+      "https://oldschool.runescape.wiki/images/Spectral_spirit_shield.png?c85b3",
+    tier: "S",
+    name: "Spectral Spirit Shield",
+    achieved: false,
+  },
+  {
+    id: 26,
+    image:
+      "https://oldschool.runescape.wiki/images/Arcane_spirit_shield.png?c85b3",
+    tier: "S",
+    name: "Arcane Spirit Shield",
+    achieved: false,
+  },
+  {
+    id: 27,
+    image:
+      "https://oldschool.runescape.wiki/images/Sanguinesti_staff.png?c85b3",
+    tier: "S",
+    name: "Sanguinesti Staff",
+    achieved: false,
+  },
+  {
+    id: 28,
+    image: "https://oldschool.runescape.wiki/images/Nightmare_staff.png?c85b3",
+    tier: "S",
+    name: "Nightmare Staff",
+    achieved: false,
+  },
+  {
+    id: 29,
+    image: "https://oldschool.runescape.wiki/images/Elidinis%27_ward.png?c85b3",
+    tier: "S+",
+    name: "Elidinis' Ward",
+    achieved: false,
+  },
+  {
+    id: 30,
+    image:
+      "https://oldschool.runescape.wiki/images/Tumeken%27s_shadow.png?c85b3",
+    tier: "S+",
+    name: "Tumeken's Shadow",
+    achieved: false,
+  },
+];
+
+export default mageEquipment;
---
+++ src/data/rangeEquipment.ts
@@ -0,0 +1,223 @@
+const rangeEquipment = [
+  {
+    id: 1,
+    image: "https://oldschool.runescape.wiki/images/Rune_crossbow.png?c85b3",
+    tier: "C",
+    name: "Rune Crossbow",
+    achieved: false,
+  },
+  {
+    id: 2,
+    image: "https://oldschool.runescape.wiki/images/Archer%27s_ring.png?c85b3",
+    tier: "B",
+    name: "Archer's Ring",
+    achieved: false,
+  },
+  {
+    id: 3,
+    image: "https://oldschool.runescape.wiki/images/Robin_hood_hat.png?c85b3",
+    tier: "B",
+    name: "Robin Hood Hat",
+    achieved: false,
+  },
+  {
+    id: 4,
+    image: "https://oldschool.runescape.wiki/images/Rangers%27_tunic.png?c85b3",
+    tier: "B",
+    name: "Rangers' Tunic",
+    achieved: false,
+  },
+  {
+    id: 5,
+    image: "https://oldschool.runescape.wiki/images/Ranger_boots.png?c85b3",
+    tier: "B",
+    name: "Ranger Boots",
+    achieved: false,
+  },
+  {
+    id: 6,
+    image: "https://oldschool.runescape.wiki/images/Ranger_gloves.png?c85b3",
+    tier: "B",
+    name: "Ranger Gloves",
+    achieved: false,
+  },
+  {
+    id: 7,
+    image: "https://oldschool.runescape.wiki/images/Odium_ward.png?c85b3",
+    tier: "B",
+    name: "Odium Ward",
+    achieved: false,
+  },
+  {
+    id: 8,
+    image: "https://oldschool.runescape.wiki/images/Dark_bow.png?c85b3",
+    tier: "B",
+    name: "Dark Bow",
+    achieved: false,
+  },
+  {
+    id: 9,
+    image: "https://oldschool.runescape.wiki/images/Twisted_buckler.png?c85b3",
+    tier: "A",
+    name: "Twisted Buckler",
+    achieved: false,
+  },
+  {
+    id: 10,
+    image: "https://oldschool.runescape.wiki/images/Heavy_ballista.png?c85b3",
+    tier: "A",
+    name: "Heavy Balista",
+    achieved: false,
+  },
+  {
+    id: 11,
+    image: "https://oldschool.runescape.wiki/images/Dragon_crossbow.png?c85b3",
+    tier: "A",
+    name: "Dragon Crossbow",
+    achieved: false,
+  },
+  {
+    id: 12,
+    image: "https://oldschool.runescape.wiki/images/Armadyl_helmet.png?c85b3",
+    tier: "S",
+    name: "Armadyl Helmet",
+    achieved: false,
+  },
+  {
+    id: 13,
+    image:
+      "https://oldschool.runescape.wiki/images/Armadyl_chestplate.png?c85b3",
+    tier: "S",
+    name: "Armadyl Chestplate",
+    achieved: false,
+  },
+  {
+    id: 14,
+    image:
+      "https://oldschool.runescape.wiki/images/Armadyl_chainskirt.png?c85b3",
+    tier: "S",
+    name: "Armadyl Chainskirt",
+    achieved: false,
+  },
+
+  {
+    id: 15,
+    tier: "S",
+    image: "https://oldschool.runescape.wiki/images/Armadyl_crossbow.png?c85b3",
+
+    name: "Armadyl Crossbow",
+    achieved: false,
+  },
+  {
+    id: 16,
+    image:
+      "https://oldschool.runescape.wiki/images/Necklace_of_anguish.png?c85b3",
+    tier: "S",
+    name: "Necklace of Anguish",
+    achieved: false,
+  },
+  {
+    id: 17,
+    image: "https://oldschool.runescape.wiki/images/Pegasian_boots.png?c85b3",
+    tier: "S",
+    name: "Pegasian Boots",
+    achieved: false,
+  },
+  {
+    id: 18,
+    image: "https://oldschool.runescape.wiki/images/Toxic_blowpipe.png?c85b3",
+    tier: "S",
+    name: "Toxic Blowpipe",
+    achieved: false,
+  },
+  {
+    id: 19,
+    image: "https://oldschool.runescape.wiki/images/Crystal_helm.png?c85b3",
+    tier: "S",
+    name: "Crystal Helm",
+    achieved: false,
+  },
+  {
+    id: 20,
+    image: "https://oldschool.runescape.wiki/images/Crystal_body.png?c85b3",
+    tier: "S",
+    name: "Crystal Body",
+    achieved: false,
+  },
+  {
+    id: 21,
+    image: "https://oldschool.runescape.wiki/images/Crystal_legs.png?c85b3",
+    tier: "S",
+    name: "Crystal Legs",
+    achieved: false,
+  },
+  {
+    id: 22,
+    image:
+      "https://oldschool.runescape.wiki/images/Bow_of_Faerdhinen.png?c85b3",
+    tier: "S",
+    name: "Bow of Faerdhinen",
+    achieved: false,
+  },
+  {
+    id: 23,
+    image:
+      "https://oldschool.runescape.wiki/images/Dragon_hunter_crossbow.png?c85b3",
+    tier: "S+",
+    name: "Dragon Hunter Crossbow",
+    achieved: false,
+  },
+  {
+    id: 24,
+    image: "https://oldschool.runescape.wiki/images/Twisted_bow.png?c85b3",
+    tier: "S+",
+    name: "Twisted Bow",
+    achieved: false,
+  },
+  {
+    id: 25,
+    image: "https://oldschool.runescape.wiki/images/Masori_mask.png?c85b3",
+    tier: "S+",
+    name: "Masori Mask",
+    achieved: false,
+  },
+  {
+    id: 26,
+    image: "https://oldschool.runescape.wiki/images/Masori_body.png?c85b3",
+    tier: "S+",
+    name: "Masori Body",
+    achieved: false,
+  },
+  {
+    id: 27,
+    image: "https://oldschool.runescape.wiki/images/Masori_chaps.png?c85b3",
+    tier: "S+",
+    name: "Masori Chaps",
+    achieved: false,
+  },
+  {
+    id: 28,
+    image: "https://oldschool.runescape.wiki/images/Zaryte_crossbow.png?c85b3",
+
+    tier: "S+",
+    name: "Zaryte Crossbow",
+    achieved: false,
+  },
+  {
+    id: 29,
+    image: "https://oldschool.runescape.wiki/images/Zaryte_vambraces.png?c85b3",
+    tier: "S+",
+    name: "Zaryte Vambraces",
+    achieved: false,
+  },
+  {
+    id: 30,
+    image:
+      "https://oldschool.runescape.wiki/images/Ava%27s_assembler.png?c85b3",
+    tier: "S+",
+    name: "Ava's Assembler",
+    achieved: false,
+  },
+];
+
+export default rangeEquipment;
